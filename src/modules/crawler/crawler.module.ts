c6923437615a6bce6c2af56f84f43c88d9481957
liuliu <voquangluu997@gmail.com>
2026-04-02 23:10:23 +0700

    add redis db

--- src/modules/crawler/crawler.module.ts
+++ src/modules/crawler/crawler.module.ts
@@ -1,29 +1,40 @@
+// src/modules/crawler/crawler.module.ts
 import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bull';
-import { ScheduleModule } from '@nestjs/schedule';
-import { PrismaModule } from '../../prisma/prisma.module';
-import { CrawlerService } from './crawler.service';
-import { CrawlerProcessor } from './processors/crawler.processor';
-import { CrawlerController } from './crawler.controller';
-import { CrawlerWebgiaService } from './sources/backup/crawler-webgia.service';
-import { Crawler24hService } from './sources/primary/crawler-24h.service';
-import { CrawlerGiavangService } from './sources/backup/crawler-giavang.service';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ScheduleModule.forRoot(),
-    BullModule.registerQueue({ name: 'crawler' }),
-    PrismaModule,
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const useTLS = configService.get('REDIS_TLS') === '1';
+        const host = configService.get('REDIS_HOST');
+        const port = configService.get('REDIS_PORT');
+        const password = configService.get('REDIS_PASSWORD');
+        
+        return {
+          redis: {
+            host,
+            port: parseInt(port, 10),
+            password,
+            tls: useTLS ? {} : undefined, 
+            retryStrategy: (times) => Math.min(times * 50, 2000),
+            maxRetriesPerRequest: 3,
+          },
+          defaultJobOptions: {
+            attempts: 3,
+            backoff: {
+              type: 'exponential',
+              delay: 5000,
+            },
+            removeOnComplete: true,
+            removeOnFail: false,
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
   ],
-  controllers: [CrawlerController],
-  providers: [
-    CrawlerService,
-    CrawlerProcessor,
-    CrawlerWebgiaService,
-    Crawler24hService,
-    CrawlerGiavangService
-  ],
-  exports: [CrawlerService],
 })
 export class CrawlerModule {}
